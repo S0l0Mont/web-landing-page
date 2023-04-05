1e29b8b8db52b6156a05a5fac5ddbd932c9b65cc
S0l0Mont <[castby9weipage@gmail.com]>
2023-04-05 11:47:02 +0700

    ⬛️ Add button

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,49 @@
 </script>
 
 <style>
-.speed-150ms {
-  animation-delay: 150ms;
-}
-
-.speed-300ms {
-  animation-delay: 300ms;
+.speed-350ms {
+  animation-delay: 350ms;
 }
 
 .speed-450ms {
   animation-delay: 450ms;
 }
+
+.speed-550ms {
+  animation-delay: 550ms;
+}
 </style>
 
 <template>
-  <div class="flex h-screen justify-center items-center bg-gray-800">
+  <div class="flex h-screen justify-center items-center bg-zinc-900">
     <div class="text-center">
-      <h1 class="text-6xl font-bold text-white animate__animated animate__fadeInDown">THIS IS A WONG</h1>
+      <h1 class="text-6xl font-bold text-white animate__animated animate__fadeInDown font-family">THIS IS A WONG</h1>
+      <img src="public\typing.gif" class="object-cover h-48 w-96 animate__animated animate__fadeIn speed-450ms">
       <div class="flex justify-center m-3 text-white animate__animated animate__fadeInDown">
-        <h2 class="mx-3 animate__animated animate__fadeIn speed-150ms">Coffee ☕</h2> <i class="fa-solid fa-slash-forward flex items-center"></i>
-        <h2 class="mx-3 animate__animated animate__fadeIn speed-300ms">Leetcode 📃</h2> <i class="fa-solid fa-slash-forward flex items-center"></i>
-        <h2 class="mx-3 animate__animated animate__fadeIn speed-450ms">Codeforces 💻</h2>
+        <h2 class="mx-3 animate__animated animate__fadeIn speed-350ms">Coffee ☕</h2> <i class="fa-solid fa-slash-forward flex items-center"></i>
+        <h2 class="mx-3 animate__animated animate__fadeIn speed-450ms">Typing 📃</h2> <i class="fa-solid fa-slash-forward flex items-center"></i>
+        <h2 class="mx-3 animate__animated animate__fadeIn speed-550ms">Leetode 💻</h2>
       </div>
       <div class="grid gap-4 grid-cols-1 grid-rows-12 md:grid-cols-3 animate__animated animate__fadeInDown">
-        <ButtonVue url="https://github.com/S0l0Mont">
+        <ButtonVue href="https://github.com/S0l0Mont" target="_blank" rel="noreferrer">
           <i class="fab fa-github mr-3"></i> 
           GitHub
         </ButtonVue>
-        <ButtonVue url="https://www.youtube.com/channel/UCLvTyyE9i0xvVGxCt7X716A">
+        <ButtonVue href="https://www.youtube.com/channel/UCgKRAftgXpOdNawFd_WC7lQ" target="_blank" rel="noreferrer">
           <i class="fab fa-youtube mr-3"></i> 
           YouTube
         </ButtonVue>
-        <ButtonVue url="mailto:wong@thisisawong.wang">
+        <ButtonVue href="mailto:wong@thisisawong.wang" target="_blank" rel="noreferrer">
           <i class="fas fa-envelope mr-3"></i> 
           E-mail
-        </ButtonVue>        
+        </ButtonVue>
+        <ButtonVue href="" target="_blank" rel="noreferrer">
+          <i class="fas fa-briefcase mr-3"></i>
+          Résumé
+        </ButtonVue>
       </div>
-      <div class="text-white my-5 animate__animated animate__fadeIn ">
-        Made with 😂 by <a href="https://m307.dev">M-307</a>
+      <div>
+
       </div>
     </div>
   </div>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -6,7 +6,7 @@
 
 <template>
     <a :href="props.url"
-        class="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-indigo-500 to-purple-400 hover:from- hover:to-purple-500 active:from-indigo-700 active:to-purple-600 focus-visible:ring ring-indigo-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2">
+        class="flex sm:inline-flex justify-center items-center  transition ease-in-out border-2 px-4 py-2 rounded border-gray-700 text-slate-400 hover:bg-gray-800">
         <slot></slot>
     </a>
 </template>
